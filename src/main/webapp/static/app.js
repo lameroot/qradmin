69596a6cfc976b4d4e4aeaefa6a14048fede41f5
Yura <smirnoff.yu@gmail.com>
2016-08-05 00:58:47 +0300

    remove ext

--- src/main/webapp/static/app.js
+++ src/main/webapp/static/app.js
@@ -6,17 +6,18 @@
 Ext.application({
     name: 'QrAdmin',
 
-    extend: 'QrAdmin.Application',
+    extend: 'QrAdmin.Application'
+	//,
 
-    requires: [
-        'QrAdmin.*'
-    ]
+    //requires: [
+    //    'QrAdmin.view.main.Main'
+    //]
 
     // The name of the initial view to create. With the classic toolkit this class
     // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
     // modern toolkit, the main view will be added to the Viewport.
     //
-    // mainView: 'QrAdmin.view.main.Main'
+    //mainView: 'QrAdmin.view.main.Main'
 	
     //-------------------------------------------------------------------------
     // Most customizations should be made to QrAdmin.Application. If you need to
