254dd7d9fc16c16dec7c8232efdb102517cea0f9
lameroot <lameroot@mail.ru>
2016-10-13 23:35:59 +0300

    test

--- src/main/java/com/qr/qradmin/config/AdminWebApplicationInitializer.java
+++ src/main/java/com/qr/qradmin/config/AdminWebApplicationInitializer.java
@@ -19,6 +19,7 @@ import java.util.EnumSet;
 
 public class AdminWebApplicationInitializer extends AbstractDispatcherServletInitializer {
 
+    
     @Override
     protected WebApplicationContext createServletApplicationContext() {
         AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
