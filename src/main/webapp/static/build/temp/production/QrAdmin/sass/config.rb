861ac2b4e094d9628b1cac0a435c46b8d4bc6845
Krainov <krainov@bpcbt.com>
2016-10-14 11:34:29 +0300

    remove from gitignore

---
+++ src/main/webapp/static/build/temp/production/QrAdmin/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'D:\Users\Krainov\Documents\workspaces_idea\my\qradmin\src\main\webapp\static/build/.sass-cache'
---
+++ src/main/webapp/static/build/temp/production/QrAdmin/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require D:\Users\Krainov\Documents\workspaces_idea\my\qradmin\src\main\webapp\static\app.js
