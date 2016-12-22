f470ce654ae65f2d325886eaf1f18629420bfd7a
lameroot <lameroot@mail.ru>
2016-12-22 23:08:30 +0300

    plays with stats

--- src/main/webapp/static/classic/src/view/statistics/StatisticsView.js
+++ src/main/webapp/static/classic/src/view/statistics/StatisticsView.js
@@ -14,7 +14,7 @@ Ext.define('PayAdmin.view.statistics.StatisticsView', {
         '->',
         {
             text: 'Refresh',
-            handler: 'onRefresh'
+            handler: 'onRefresh1'
         }
     ],
     items: [
@@ -97,44 +97,59 @@ Ext.define('PayAdmin.view.statistics.StatisticsView', {
                     legend: {
                         docked: 'right'
                     },
+                    //store: {
+                    //    type: 'statistic'
+                    //},
+
                     store: {
-                        type: 'statistic'
-                    },
-                    /*
-                    store: {
-                        fields: ['month', 'data1', 'data2', 'data3', 'data4', 'other'],
+                        fields: ['month', 'data1'],
                         data: [
-                            { month: 'Jan', data1: 20, data2: 37, data3: 35, data4: 4, other: 4 },
-                            { month: 'Feb', data1: 20, data2: 37, data3: 36, data4: 5, other: 2 },
-                            { month: 'Mar', data1: 19, data2: 36, data3: 37, data4: 4, other: 4 },
-                            { month: 'Apr', data1: 18, data2: 36, data3: 38, data4: 5, other: 3 },
-                            { month: 'May', data1: 18, data2: 35, data3: 39, data4: 4, other: 4 },
-                            { month: 'Jun', data1: 17, data2: 34, data3: 42, data4: 4, other: 3 },
-                            { month: 'Jul', data1: 16, data2: 34, data3: 43, data4: 4, other: 3 },
-                            { month: 'Aug', data1: 16, data2: 33, data3: 44, data4: 4, other: 3 },
-                            { month: 'Sep', data1: 16, data2: 32, data3: 44, data4: 4, other: 4 },
-                            { month: 'Oct', data1: 16, data2: 32, data3: 45, data4: 4, other: 3 },
-                            { month: 'Nov', data1: 15, data2: 31, data3: 46, data4: 4, other: 4 },
-                            { month: 'Dec', data1: 15, data2: 31, data3: 47, data4: 4, other: 3 }
+                            { month: 'Jan', data1: 20},
+                            { month: 'Feb', data1: 20},
+                            { month: 'Mar', data1: 19},
+                            { month: 'Apr', data1: 18},
+                            { month: 'May', data1: 18},
+                            { month: 'Jun', data1: 17},
+                            { month: 'Jul', data1: 16},
+                            { month: 'Aug', data1: 16},
+                            { month: 'Sep', data1: 16},
+                            { month: 'Oct', data1: 16},
+                            { month: 'Nov', data1: 15},
+                            { month: 'Dec', data1: 1}
                         ]
                     },
-                    */
+
                     insetPadding: 40,
 
-                    axes: [{
+                    axes: [
+                        {
                         type: 'numeric',
-                        //fields: ['data1', 'data2', 'data3', 'data4'],
+                        fields: ['data1','data2'],
                         position: 'left',
                         grid: true,
                         minimum: 0
                     }, {
                         type: 'category',
-                        fields: 'name',
+                        fields: 'month',
                         position: 'bottom',
                         grid: true
-                    }],
+                    }
+                    ],
                     series: [
+                        {
+                            type: 'line',
+                            title: 'IE',
+                            xField: 'month',
+                            yField: 'data1'
 
+                        },
+                        //{
+                        //    //type: 'line',
+                        //    //title: 'IE2',
+                        //    //xField: 'month',
+                        //    //yField: 'data2'
+                        //
+                        //}
                     ]
                 }
             ]
--- src/main/webapp/static/classic/src/view/statistics/StatisticsViewController.js
+++ src/main/webapp/static/classic/src/view/statistics/StatisticsViewController.js
@@ -41,82 +41,99 @@ Ext.define('PayAdmin.view.statistics.StatisticsViewController', {
         chart.preview();
     },
 
+    onRefresh1: function () {
+        //var chart = this.getView().down('#chart');
+        //var chartStore = chart.getStore();
+        var chart = this.getView().down('#chart'),
+            chartStore = chart.getStore();
+
+        var fields = ['month', 'data1', 'data2'];
+        chartStore.setFields(fields);
+
+        var data =[
+            { month: 'Jan', data1: 10, data2: 10},
+            { month: 'Feb', data1: 10, data2: 10},
+            { month: 'Mar', data1: 9, data2: 10},
+            { month: 'Apr', data1: 8, data2: 10},
+            { month: 'May', data1: 18, data2: 10},
+            { month: 'Jun', data1: 17, data2: 10},
+            { month: 'Jul', data1: 16, data2: 10},
+            { month: 'Aug', data1: 16, data2: 10},
+            { month: 'Sep', data1: 16, data2: 10},
+            { month: 'Oct', data1: 16, data2: 10},
+            { month: 'Nov', data1: 15, data2: 10},
+            { month: 'Dec', data1: 10, data2: 10}
+        ];
+
+
+
+
+        var series = [];
+        //todo:  не отображается 2 линия(((
+        var newSeries = new Ext.chart.series.Line(
+            {
+            chart: chart,
+            type: 'line',
+            title: 'IE2',
+            xField: 'month',
+            yField: 'data2'
+        });
+        series.push(newSeries);
+        chart.series = series;
+
+        chartStore.setData(data);
+        debugger;
+
+
+
+
+    },
+
     onRefresh: function () {
+        /*
         var chart = this.getView().down('#chart');
         var chartStore = chart.getStore();
 
         var templates = ['name', '1','2','3'];
 
-        var axes = chart.getAxes();
-        axes[0].setFields(['1','2','3'])
-
-        var series = chart.getSeries();
-        debugger;
-        series.add({
-            type: 'line',
-            title: 'IE',
-            xField: 'name',
-            yField: '1'
-        });
-        series.add({
-            type: 'line',
-            title: 'IE',
-            xField: 'name',
-            yField: '2'
-        });
-        series.add({
-            type: 'line',
-            title: 'IE',
-            xField: 'name',
-            yField: '3'
-        });
-
         chartStore.setFields(templates);
         chartStore.getProxy().setExtraParam('filter', Ext.JSON.encode(templates));
         chartStore.load();
 
+        debugger;
 
+        var axes = chart.getAxes();
+        axes[0].setFields(['1','2','3'])
 
-        /*
-        var chart = this.lookupReference('chart'),
-            store = chart.getStore();
-
-        var store = Ext.create('Ext.data.Store',{
-            extend      : 'Ext.data.Store',
-
-            proxy: {
-                type: 'ajax',
-                url: 'api/statistics',
-                //extraParams: {
-                //    size: 10,
-                //    filter: {}
-                //},
-                reader: {
-                    type: 'json',
-                    root: 'data'
-                }
+        var series = [];
+        series.push({
+            type: 'line',
+            title: 'IE2',
+            xField: 'name',
+            yField: '1'
+        },
+            {
+                type: 'line',
+                title: 'IE2',
+                xField: 'name',
+                yField: '2'
             },
-            autoLoad: false
-        });
-
-        Ext.Ajax.request({
-            type: 'ajax',
-            url: 'api/statistics',
-            method: 'GET',
-            jsonData: {
-                size: 10,
-                filter: {}
-
-            },
-            success: function(response) {
-                var data = Ext.decode(response.responseText);
-            },
-            failure: function(response){
-                Ext.Msg.alert('GascardPay',response);
+            {
+                type: 'line',
+                title: 'IE2',
+                xField: 'name',
+                yField: '3'
             }
-        });
+
+        );
+
+        chart.config.series.push(series);
+
+
+        chart.redraw();
+        //chart.refresh();
         */
-        //chartStore.refreshData();
+
     }
 
 
